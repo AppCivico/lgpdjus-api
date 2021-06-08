34ecfcb7469c521a2065e787d44ab78d1de192df
Renato Cron <renato.cron@gmail.com>
2021-06-08 16:48:24 -0300

    adicionar svg

--- api/lib/Lgpdjus/Schema/Result/Ticket.pm
+++ api/lib/Lgpdjus/Schema/Result/Ticket.pm
@@ -165,6 +165,18 @@ sub html_preview {
           '<div style="color: #646464; font-size: 12pt; padding: 12pt; text-align: center"> Sua solicitação está em andamento, clique aqui para visualizar os detalhes da solicitação.</div>';
     }
 
+
+        $preview .= sprintf
+          '<div style="color: #398FCE; font-size: 10pt; padding: 12pt; text-align: right"> Visualizar.
+<svg width="20" height="20" viewBox="0 0 20 20">
+  <title>
+    external link
+  </title>
+  <path d="M17 17H3V3h5V1H3a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5h-2z"/>
+  <path d="M19 1h-8l3.29 3.29-5.73 5.73 1.42 1.42 5.73-5.73L19 9V1z"/>
+</svg>
+</div>';
+
     $preview =~ s/<br\/>$//;
 
     return $preview;
