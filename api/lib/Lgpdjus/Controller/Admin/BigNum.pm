116f6db21be75c647be851ea615cd9b77f660a09
Renato Cron <renato.cron@gmail.com>
2021-06-29 10:58:37 -0300

    metabase

--- api/lib/Lgpdjus/Controller/Admin/BigNum.pm
+++ api/lib/Lgpdjus/Controller/Admin/BigNum.pm
@@ -5,6 +5,8 @@ use JSON;
 use Lgpdjus::Utils;
 use DateTime;
 use MooseX::Types::Email qw/EmailAddress/;
+use Crypt::JWT qw(encode_jwt decode_jwt);
+use Mojo::URL;
 
 sub abignum_get {
     my $c = shift;
@@ -30,14 +32,41 @@ sub abignum_get {
         push @results, $r;
     }
 
+    my @rows = (
+        {name => 'Resumo manifestantes', resource => {dashboard => 1}, params => {}},
+    );
+    my $metabase_secret = $ENV{METABASE_SECRET} || 'secret';
+    my @ret             = ();
+    foreach my $payload (@rows) {
+        $payload->{_}{admin_user} = $c->stash('admin_user')->id;
+        $payload->{exp} = time() + 3600;                           # 1 hour
+
+        my $jwt = encode_jwt(
+            alg     => 'HS256',
+            key     => $metabase_secret,
+            payload => $payload
+        );
+        my $url = Mojo::URL->new('https://lgpdjus-metabase.appcivico.com/');
+        $url->path('/embed/dashboard/' . $jwt);
+        $url->fragment('bordered=false&titled=false');
+
+        push @ret, {
+            name => $payload->{name},
+            url  => $url->to_string(),
+        };
+    }
+
     return $c->respond_to_if_web(
         json => {
             json => {
                 results => \@results,
+                reports => \@ret,
             }
         },
         html => {
             results => \@results,
+            reports => \@ret,
+
         },
     );
 }
