536833ae40bf9cee640ea40867186e909102a6df
Renato Cron <renato.cron@gmail.com>
2021-07-13 17:27:20 -0300

    remove update cep

--- api/lib/Lgpdjus/Controller/Maintenance.pm
+++ api/lib/Lgpdjus/Controller/Maintenance.pm
@@ -48,30 +48,6 @@ sub housekeeping {
         $r->update({deletion_started_at => \'now()'});
         $ENV{LAST_DELETE_JOB_ID} = $job_id;
     }
-    if (!is_test()) {
-        my $update_cep = $c->schema->resultset('Cliente')->search(
-            {
-                cep_estado => undef,
-            },
-            {
-                columns => ['id'],
-            }
-        );
-        while (my $r = $update_cep->next) {
-
-            my $job_id = $minion->enqueue(
-                'cliente_update_cep',
-                [
-                    $r->id,
-                ] => {
-                    attempts => 5,
-                }
-            );
-
-            slog_info('Adding job cliente_update_cep %s, job id %s', $r->id, $job_id);
-            $r->update({cep_estado => ''});
-        }
-    }
 
     return $c->render(json => {});
 }
